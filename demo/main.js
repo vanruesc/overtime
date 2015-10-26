6bdce13ce9ab984d49f356e31ab4531dd9a1f394
Raoul v. R <vanruesc@outlook.de>
2015-10-26 23:17:02 +0100

    Modified demo code.

--- demo/main.js
+++ demo/main.js
@@ -1,23 +1,15 @@
-/**
- * Usage Demo:
- *  How to set up and use Overtime.
- *
- * @author Raoul van Rueschen
- * @version 1.0.0, 01.06.2015
- */
-
 "use strict";
 
 // Create an Overtime instance.
 var overtime = new Overtime({
- time: 2,
- timeMeasure: Overtime.TimeMeasure.MINUTES,
- size: [13, 37]
+	time: 2,
+	timeMeasure: Overtime.TimeMeasure.MINUTES,
+	size: [13, 37]
 });
 
 /*
  * In order to append the canvas of Overtime to your 
- * website, you'll have to listen for the document's
+ * website, you'll have to wait for the document's
  * "DOMContentLoaded"-event.
  *
  * If you want to rely on the sizes of DOM elements 
@@ -25,59 +17,64 @@ var overtime = new Overtime({
  * "load"-event of the window object instead.
  */
 
-window.addEventListener("load", function init()
-{
- var container = document.getElementById("container"),
-  t = document.getElementById("t"),
-  tm = document.getElementById("tm");
+window.addEventListener("load", function init() {
 
- // Grab the output canvas and put it on the page.
- container.appendChild(overtime.canvas);
+	var container = document.getElementById("container");
+	var t = document.getElementById("t");
+	var tm = document.getElementById("tm");
 
- function resize()
- {
-  var min = (container.offsetWidth < container.offsetHeight) ? container.offsetWidth : container.offsetHeight;
+	// Grab the output canvas and put it on the page.
+	container.appendChild(overtime.canvas);
 
-  // Set the size of the canvas: [width, height].
-  min -= 10;
-  overtime.size = [min, min];
- }
+	function resize() {
 
- // Set the size right now.
- resize();
+		var min = (container.offsetWidth < container.offsetHeight) ? container.offsetWidth : container.offsetHeight;
 
- // And handle window resize events.
- window.addEventListener("resize", resize);
+		// Set the size of the canvas: [width, height].
+		min -= 10;
+		overtime.size = [min, min];
 
- // Do something when the time elapsed.
- overtime.addEventListener("elapsed", function()
- {
-  document.getElementById("textualOutput").innerHTML = "Time's up!";
- });
+	}
 
- // Make use of Overtime's "update"-event.
- overtime.addEventListener("update", function()
- {
-  document.getElementById("textualOutput").innerHTML = overtime.time;
- });
+	// Set the size right now.
+	resize();
 
- // Control Overtime.
- document.getElementById("stop").addEventListener("click", function() { overtime.stop(); });
- document.getElementById("start").addEventListener("click", function() { overtime.start(); });
- document.getElementById("rewind").addEventListener("click", function() { overtime.rewind(); });
- document.getElementById("rewindBy").addEventListener("click", function() { overtime.rewindBy(parseInt(t.value)); });
- document.getElementById("advanceBy").addEventListener("click", function() { overtime.advanceBy(parseInt(t.value)); });
- document.getElementById("prolongBy").addEventListener("click", function() { overtime.prolongBy(parseInt(t.value)); });
- document.getElementById("shortenBy").addEventListener("click", function() { overtime.shortenBy(parseInt(t.value)); });
- document.getElementById("set").addEventListener("click", function() { overtime.time = parseInt(t.value); });
- tm.addEventListener("change", function() { overtime.tm = parseInt(tm.value); });
+	// And handle window resize events.
+	window.addEventListener("resize", resize);
 
- // Temporary ad hoc solution.
- document.getElementById("request").addEventListener("click", function()
- {
-  document.getElementById("qr").src = "https://caeb-fhbrandenburg.rhcloud.com/v1/qr?uid=" + document.getElementById("uid").value;
- });
+	// Do something when the time elapsed.
+	overtime.addEventListener("elapsed", function() {
 
- // Clean up.
- window.removeEventListener("load", init);
+		document.getElementById("textualOutput").innerHTML = "Time's up!";
+
+	});
+
+	// Make use of Overtime's "update"-event.
+	overtime.addEventListener("update", function() {
+
+		document.getElementById("textualOutput").innerHTML = overtime.time;
+
+	});
+
+	// Control Overtime.
+	document.getElementById("stop").addEventListener("click", function() { overtime.stop(); });
+	document.getElementById("start").addEventListener("click", function() { overtime.start(); });
+	document.getElementById("rewind").addEventListener("click", function() { overtime.rewind(); });
+	document.getElementById("rewindBy").addEventListener("click", function() { overtime.rewindBy(parseInt(t.value)); });
+	document.getElementById("advanceBy").addEventListener("click", function() { overtime.advanceBy(parseInt(t.value)); });
+	document.getElementById("prolongBy").addEventListener("click", function() { overtime.prolongBy(parseInt(t.value)); });
+	document.getElementById("shortenBy").addEventListener("click", function() { overtime.shortenBy(parseInt(t.value)); });
+	document.getElementById("set").addEventListener("click", function() { overtime.time = parseInt(t.value); });
+	tm.addEventListener("change", function() { overtime.tm = parseInt(tm.value); });
+
+	// Temporary ad hoc solution.
+	document.getElementById("request").addEventListener("click", function() {
+
+		document.getElementById("qr").src = "https://caeb-fhbrandenburg.rhcloud.com/v1/qr?uid=" + document.getElementById("uid").value;
+
+	});
+
+	// Clean up.
+	window.removeEventListener("load", init);
+
 });
